e65e5b6ac3455bf03d8557d95a71050b8045293e
Dave Hoy <hoyski@gmail.com>
2020-09-25 07:09:23 -0400

    Added pseudocode for PersonManagerApp

--- ch08_proj2_PersonManager/src/Employee.java
+++ ch08_proj2_PersonManager/src/Employee.java
@@ -1,4 +1,30 @@
 
 public class Employee extends Person {
 
+	// Fields
+	private String ssn;
+
+	// Constructors
+	public Employee(String firstName, String lastName, String ssn) {
+		super(firstName, lastName);
+		this.ssn = ssn;
+	}
+
+	// Getter and setters
+	public String getSsn() {
+		return ssn;
+	}
+
+	public void setSsn(String ssn) {
+		this.ssn = ssn;
+	}
+
+	// Overrides
+	@Override
+	public String toString() {
+		String maskedSsn = "xxx-xxxx-" + ssn.substring(ssn.length() - 4);
+
+		return super.toString() + "\nSSN: " + maskedSsn;
+	}
+
 }
---
+++ ch08_proj2_PersonManager/src/PersonManagerApp.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class PersonManagerApp {
+	public static void main(String[] args) {
+		
+		//
+		// Declare a Person reference variable
+		//
+		// while the user wants to continue {
+		//   prompt the user to enter a C or E
+		//   if the user didn't enter a C or E {
+		//     output an error message
+		//     use the continue; keyword to go back to the top of the loop
+		//   }
+		//   prompt the user for a first name
+		//   prompt the user for a last name
+		//   if the user entered C {
+		//     declare a new Customer instance
+		//     prompt the user for a customer number
+		//     set the customer number into the Customer instance
+		//     set the Person reference declared before the loop equal to the Customer instance
+		//   }
+		//   else {
+		//     declare a new Employee instance
+		//     prompt the user for an SSN
+		//     set the SSN into the Employee instance
+		//     set the Person reference declared before the loop equal to the Employee instance
+		//   }
+		//   set the first name into the Person reference
+		//   set the last name into the Person reference
+		//   print the Person reference
+		//
+		//   prompt the user whether they want to continue
+		// }
+	}
+}
