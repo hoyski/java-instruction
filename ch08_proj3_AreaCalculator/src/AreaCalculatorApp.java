29f964b171715ab9106c234a231a24cc48f456ea
Dave Hoy <hoyski@gmail.com>
2020-09-28 21:52:58 -0400

    Reverted AreaCalculatorApp changes

--- ch08_proj3_AreaCalculator/src/AreaCalculatorApp.java
+++ ch08_proj3_AreaCalculator/src/AreaCalculatorApp.java
@@ -12,47 +12,37 @@ public class AreaCalculatorApp {
 		while (choice.equalsIgnoreCase("y")) {
 			String csr = Console.getString("Calculate area of circle, square or rectange? (c/s/r)");
 
-			if (csr.equalsIgnoreCase("c")) {
+			switch (csr) {
+			case "c":
+			case "C":
 				double radius = Console.getDouble("Enter radius: ");
 				Circle circle = new Circle(radius);
 				printArea("Circle", circle);
-			} else if (csr.equalsIgnoreCase("s")) {
+
+				break;
+
+			case "s":
+			case "S":
 				double squareWidth = Console.getDouble("Enter width: ");
 				Square square = new Square(squareWidth);
 				printArea("Square", square);
-			}
 
-//			switch (csr) {
-//			case "c":
-//			case "C":
-//				double radius = Console.getDouble("Enter radius: ");
-//				Circle circle = new Circle(radius);
-//				printArea("Circle", circle);
-//
-//				break;
-//
-//			case "s":
-//			case "S":
-//				double squareWidth = Console.getDouble("Enter width: ");
-//				Square square = new Square(squareWidth);
-//				printArea("Square", square);
-//
-//				break;
-//
-//			case "r":
-//			case "R":
-//				double rectangleWidth = Console.getDouble("Enter width: ");
-//				double height = Console.getDouble("Enter height: ");
-//				Rectangle rectangle = new Rectangle(rectangleWidth, height);
-//				rectangle.setHeight(17.0);
-//				printArea("Rectangle", rectangle);
-//
-//				break;
-//				
-//			default:
-//				System.out.println("Input is invalid");
-//				break;
-//			}
+				break;
+
+			case "r":
+			case "R":
+				double rectangleWidth = Console.getDouble("Enter width: ");
+				double height = Console.getDouble("Enter height: ");
+				Rectangle rectangle = new Rectangle(rectangleWidth, height);
+				rectangle.setHeight(17.0);
+				printArea("Rectangle", rectangle);
+
+				break;
+
+			default:
+				System.out.println("Input is invalid");
+				break;
+			}
 
 			choice = Console.getString("Continue? (y/n) ");
 		}
