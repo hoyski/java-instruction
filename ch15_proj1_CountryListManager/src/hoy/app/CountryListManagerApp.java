2856320e154e76f1309f734f403c567400c3d7e6
Dave Hoy <hoyski@gmail.com>
2020-10-17 11:57:40 -0400

    2020-10-17 Instruction

--- ch15_proj1_CountryListManager/src/hoy/app/CountryListManagerApp.java
+++ ch15_proj1_CountryListManager/src/hoy/app/CountryListManagerApp.java
@@ -1,64 +1,41 @@
 package hoy.app;
 
-import java.util.List;
+import java.util.ArrayList;
 
 import hoy.business.CountriesIO;
 
 public class CountryListManagerApp {
 
-	private static final int COMMAND_LIST = 1;
-	private static final int COMMAND_ADD = 2;
-	private static final int COMMAND_EXIT = 3;
+	public static void main(String[] args) {
 
-	private static void printWelcomeMessage() {
+		CountriesIO countriesIO = new CountriesIO();
+
 		System.out.println("Country List Manager");
 		System.out.println();
 		System.out.println("COMMAND MENU");
-		System.out.println(COMMAND_LIST + " - List countries");
-		System.out.println(COMMAND_ADD + " - Add country");
-		System.out.println(COMMAND_EXIT + " - Exit");
-	}
+		System.out.println("1 - List countries");
+		System.out.println("2 - Add country");
+		System.out.println("3 - Exit");
 
-	private static void listCountries() {
-		CountriesIO countriesIO = new CountriesIO();
-		List<String> countries = countriesIO.getCountries();
-
-		for (String country : countries) {
-			System.out.println(country);
-		}
-	}
-
-	private static void addCountry() {
-
-	}
-
-	public static void main(String[] args) {
-		printWelcomeMessage();
-
-		int command = -1;
-		while (command != COMMAND_EXIT) {
-			System.out.println();
+		int command = 0;
+		while (command != 3) {
 			command = Console.getInt("Enter menu number: ");
-			System.out.println();
 
-			switch (command) {
-			case COMMAND_LIST:
-				listCountries();
-				break;
-
-			case COMMAND_ADD:
-				addCountry();
-				break;
-
-			case COMMAND_EXIT:
-				System.out.println("Goodbye.");
-				break;
-
-			default:
-				System.out.println("Invalid menu number");
-				break;
+			if (command == 1) {
+				ArrayList<String> countries = countriesIO.getCountries();
+				for (String country : countries) {
+					System.out.println(country);
+				}
+			} else if (command == 2) {
+				String newCountry = Console.getString("Enter country: ");
+				ArrayList<String> countries = countriesIO.getCountries();
+				countries.add(newCountry);
+				if (countriesIO.saveCountries(countries)) {
+					System.out.println("This country has been saved.");
+				}
 			}
 		}
+
 	}
 
 }
--- ch15_proj1_CountryListManager/src/hoy/business/CountriesIO.java
+++ ch15_proj1_CountryListManager/src/hoy/business/CountriesIO.java
@@ -1,33 +1,44 @@
 package hoy.business;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.List;
 
 public class CountriesIO {
 
-	private final String inputFilename = "countries.txt";
+	private final String countriesFilename = "countries.txt";
 
-	public List<String> getCountries() {
+	public ArrayList<String> getCountries() {
 
-		List<String> countries = new ArrayList<>();
+		ArrayList<String> countries = new ArrayList<>();
 
-		try (BufferedReader in = new BufferedReader(new FileReader(inputFilename))) {
-			String country = in.readLine();
-			while (country != null) {
-				countries.add(country);
-				country = in.readLine();
+		try (BufferedReader in = new BufferedReader(new FileReader(countriesFilename))) {
+			String line = in.readLine();
+			while (line != null) {
+				countries.add(line);
+				line = in.readLine();
 			}
 		} catch (IOException e) {
-			System.err.println("Caught IOException in getCountries(). Msg: " + e.getMessage());
+			System.err.println("Caught IOException. Msg: " + e.getMessage());
 		}
 
 		return countries;
 	}
 
-	public boolean saveCountries(List<String> countries) {
+	public boolean saveCountries(ArrayList<String> countries) {
+		try (PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(countriesFilename)))) {
+			for (String country : countries) {
+				out.println(country);
+			}
+		} catch (IOException e) {
+			System.err.println("Caught IOException. Msg: " + e.getMessage());
+			return false;
+		}
+
 		return true;
 	}
 
